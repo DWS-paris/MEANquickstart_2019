03beb7252e8fc1fd26cdd576ea56e87c340dac19
DWS-paris <j.noyer@digitalworkshop.fr>
2019-12-06 15:18:35 +0100

    First commit

---
+++ models/index.js
@@ -0,0 +1,14 @@
+/* 
+Definition
+*/
+    const Models = {
+        identity: require('./identity.model'),
+        user: require('./user.model'),
+    };
+//
+
+/* 
+Export
+*/
+    module.exports = Models;
+//
---
+++ services/mandatory.service.js
@@ -0,0 +1,14 @@
+/* 
+Definition
+*/
+    const Mandatories = {
+        register: ['email', 'password', 'firstname', 'lastname'],
+        login: ['email', 'password'],
+    };
+//
+
+/* 
+Export
+*/
+    module.exports = Mandatories;
+//
